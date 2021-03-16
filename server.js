365e12c9406ea70afc4d4ea4a3052a3ea87c7693
dkwon253 <78809306+dkwon253@users.noreply.github.com>
2021-03-16 13:10:12 -0700

    pokemon generator

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // IMPORT
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const fetch = require("node-fetch");
 
 // INSTANCE
 const app = express();
@@ -34,6 +35,74 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client
     app.get('/', (req, res) => {
         res.render('index.ejs')
     })
+
+    // app.get("/user", (req,res) => {
+    //     fetch("https://pokeapi.co/api/v2/pokemon/" +randomPokemonGenerator())
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         const pokemon = {
+    //             id: data.id,
+    //             name: data.name,
+    //             type: data.types.map((type) => type.type.name),
+    //             moves: [data.moves[0].move.name, data.moves[1].move.name, data.moves[2].move.name, data.moves[3].move.name],
+    //             hp: data.stats[0].base_stat,
+    //             attack: data.stats[1].base_stat,
+    //             defense: data.stats[2].base_stat
+    //         };
+    //         console.log(pokemon);
+    //     })
+    //     .catch(error => { console.error(error)})
+    //     res.status(200).send({ msg: "Success!"})
+    // })
+
+    // app.get("/user", (req,res) => {
+    //     for (let i=0; i<5; i++) {
+    //         fetch("https://pokeapi.co/api/v2/pokemon/" +randomPokemonGenerator())
+    //         .then((res) => {
+    //             return res.json();
+    //         })
+    //         .then((data) => {
+    //             const pokemon = {
+    //                 id: data.id,
+    //                 name: data.name,
+    //                 type: data.types.map((type) => type.type.name),
+    //                 moves: [data.moves[0].move.name, data.moves[1].move.name, data.moves[2].move.name, data.moves[3].move.name],
+    //                 hp: data.stats[0].base_stat,
+    //                 attack: data.stats[1].base_stat,
+    //                 defense: data.stats[2].base_stat
+    //             };
+    //             console.log(pokemon)
+    //         })
+    //         .catch(error => { console.error(error)})
+    //         res.status(200).send({ msg: "Success!"});
+    //     }
+    // })
+
+    app.get("/user", (req, res) => {
+        const promises = [];
+        for (let i = 0; i < 6; i++) {
+            const url = `https://pokeapi.co/api/v2/pokemon/${randomPokemonGenerator()}` 
+            promises.push(fetch(url).then((res) => res.json()));
+        }
+        Promise.all(promises).then(results => {
+            const pokemon = results.map ((data) => ({
+                id: data.id,
+                name: data.name,
+                type: data.types.map((type) => type.type.name),
+                moves: [ data.moves[0].move.name, data.moves[1].move.name, data.moves[2].move.name, data.moves[3].move.name,],
+                hp: data.stats[0].base_stat,
+                attack: data.stats[1].base_stat,
+                defense: data.stats[2].base_stat,
+            }))
+            console.log(pokemon)
+        });
+        res.status(200).send({ msg: "Success!"})
+    })
+
+    // function to call a random pokemon from the API 
+    function randomPokemonGenerator () {
+        return Math.floor(Math.random() * 150)
+    }
 
     // SERVER LISTENING
     app.listen(port, () => {

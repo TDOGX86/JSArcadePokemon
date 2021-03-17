59a5b6c27a913b7bd56c11fc8f2e5cf620e81080
dkwon253 <78809306+dkwon253@users.noreply.github.com>
2021-03-17 11:10:34 -0700

    fixed routes

--- app/config/database.js
+++ app/config/database.js
@@ -2,9 +2,7 @@ const username = "arcade";
 const pass = "bootcamp2020b!";
 const collection = "arcade";
 
-const connectionString = `mongodb+srv://dkwon23:HelloWorld!@cluster0.hvpzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-// const connectionString = `mongodb+srv://${username}:${pass}@cluster0.56mug.mongodb.net/${collection}?retryWrites=true&w=majority`; // Cluster might need to change
+const connectionString = `mongodb+srv://${username}:${pass}@cluster0.56mug.mongodb.net/${collection}?retryWrites=true&w=majority`; // Cluster might need to change
 
 module.exports = {
   url: connectionString,

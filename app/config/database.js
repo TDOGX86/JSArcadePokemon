c0e7149c2ea29956eec63b4bdad510c4e251296c
dkwon253 <78809306+dkwon253@users.noreply.github.com>
2021-03-16 22:27:08 -0700

    deleted db

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,6 @@
 const username = 'arcade';
 const pass = 'bootcamp2020b!';
 const collection = 'arcade';
-// const connectionString = `mongodb+srv://Shawn_charles:a90db52b@cluster0.ctxg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const connectionString = `mongodb+srv://${username}:${pass}@cluster0.56mug.mongodb.net/${collection}?retryWrites=true&w=majority`; // Cluster might need to change
 

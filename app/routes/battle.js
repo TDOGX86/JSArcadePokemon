d5158e7ef90e9121f7164104d55d45aeb390dd0f
chaashaw <shawn.charles@tlgcohort.com>
2021-03-17 03:40:38 -0400

    start setting teams

--- app/routes/battle.js
+++ app/routes/battle.js
@@ -1,18 +1,9 @@
 let battleSchema = require("../models/battle");
 let teamSchema = require("../models/team");
+const fetch = require("node-fetch");
 module.exports = function (app, passport, db) {
   app.get("/battle", (req, res) => {
-    console.log();
-    console.table(Object.entries(req.headers));
-    console.table(Object.entries(req.session));
-    app.get("/randomOpponent", (req2, res2) => {
-      teamSchema.findOneAndUpdate(
-        { email: req.user.local.email },
-        {
-          email: req.user.local.email,
-          pokemon: res2,
-        }
-      );
-    });
+    console.log("here");
+    res.status(200).send({ msg: "Success!" });
   });
 };

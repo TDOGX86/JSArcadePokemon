4ca246d751a73ac75dd7472995172041008c41f0
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-03-19 09:14:31 -0400

    Comestic changes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // IMPORTS
-const express      = require("express");
-const mongoose     = require("mongoose");
-const passport     = require("passport");
-const flash        = require("connect-flash");
-const morgan       = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const session      = require("express-session");
-const path         = require('path')
-const configDB     = require("./app/config/database.js");
+const session = require("express-session");
+const path = require("path");
+const configDB = require("./app/config/database.js");
 
 // INSTANCE
 const app = express();
@@ -16,32 +16,37 @@ const app = express();
 const port = process.env.PORT || 8100;
 
 // DATABASE
-mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, database) => {
+mongoose.connect(
+  process.env.DATABASE_URL || configDB.url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, database) => {
     if (err) return console.error(err);
 
-    const db = database
-    
-    require("./app/config/passport")(passport);   // passport configuration
-    
+    const db = database;
+
+    require("./app/config/passport")(passport); // passport configuration
+
     // RENDER ENGINE
-    app.set('views', './views');
-    app.set("view engine", "ejs");                // set up ejs for templating
+    app.set("views", "./views");
+    app.set("view engine", "ejs"); // set up ejs for templating
 
     // MIDDLEWARE
     //app.use(morgan("dev"));                     // log every request to the console
-    app.use(cookieParser());                      // read cookies (needed for auth)
-    app.use(express.json());                      // get information from html forms
+    app.use(cookieParser()); // read cookies (needed for auth)
+    app.use(express.json()); // get information from html forms
     app.use(express.urlencoded({ extended: true }));
-    app.use(express.static("public"));            // access public resources
-    app.use(session({                             // passport sessions
-        secret: "pokEmon",                        // session secret...DONT TELL ANYONE
+    app.use(express.static("public")); // access public resources
+    app.use(
+      session({
+        // passport sessions
+        secret: "pokEmon", // session secret...DONT TELL ANYONE
         resave: true,
         saveUninitialized: true,
       })
     );
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(flash());                             //  for flash messages in session
+    app.use(flash()); //  for flash messages in session
 
     // CRUD ACCESS ROUTES
     require("./app/routes/main.js")(app, passport, db);
@@ -49,6 +54,6 @@ mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true
     require("./app/routes/battle.js")(app, passport, db);
   }
 );
-  
+
 app.listen(port);
 console.log("The magic happens on port " + port);
